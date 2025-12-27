410182e5c3af1a208246a22384b39b130cbb69e0
lrks89 <kendoka89@gmail.com>
2025-12-27 01:41:35 -0500

    removed animation system

--- src/main/java/net/wowmod/WeaponsOfWarClient.java
+++ src/main/java/net/wowmod/WeaponsOfWarClient.java
@@ -3,19 +3,13 @@ package net.wowmod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.minecraft.server.packs.PackType;
-import net.wowmod.client.animation.ModAnimations;
 import net.wowmod.client.renderer.ThrownWeaponRenderer;
-import net.wowmod.entity.ThrownWeaponEntity;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 @Environment(EnvType.CLIENT)
 public class WeaponsOfWarClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Fix: Register the listener to load animations when resources are ready (avoids NPE crash)
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new ModAnimations());
 
         // Register Renderers
         EntityRendererRegistry.register(WeaponsOfWar.THROWN_WEAPON, ThrownWeaponRenderer::new);
--- src/main/java/net/wowmod/util/IModAnimationPlayer.java
+++
@@ -1,9 +0,0 @@
-package net.wowmod.util;
-
-import net.minecraft.world.entity.AnimationState;
-
-public interface IModAnimationPlayer {
-    AnimationState wowmod$getIdleAnimationState();
-    AnimationState wowmod$getWalkAnimationState();
-    AnimationState wowmod$getSprintAnimationState();
-}
